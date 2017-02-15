8f0769b1f0c8b78168a42d72ee3ac503c92c92f9
Drew Bronson <bronsonboy96@gmail.com>
2017-02-15 13:38:27 -0600

    Choice working for Nobles.

--- source/noble.rkt
+++ source/noble.rkt
@@ -1,13 +1,19 @@
 #lang rosette
 
-(require "nondeterminism.rkt")
+(require rosette/lib/angelic)
 
 (provide noble make-noble print-noble)
 
 (struct noble (name gender house) #:transparent)
 
+;; (define (make-noble names genders houses)
+;;   (random-instance noble (list names genders houses)))
+
 (define (make-noble names genders houses)
-  (random-instance noble (list names genders houses)))
+  (noble (car (choose* (shuffle names)))
+         (car (choose* (shuffle genders)))
+         (car (choose* (shuffle houses)))))
+
 
 (define (print-noble noble)
   (string-append (noble-name noble) " of the " (noble-house noble)))
--- source/sandbox.rkt
+++ source/sandbox.rkt
@@ -1,28 +1,39 @@
 #lang rosette
 
 (require rosette/lib/angelic)
+(require rosette/lib/synthax)
 
-(define choices '(0 1 2 3 4 5 6 7 8 9))
+;;(define choices '(0 1 2 3 4 5 6 7 8 9))
+(define choices '(0))
 
-(define (test-pick-one)
-  (clear-asserts!)
-  (define-symbolic* x integer?)
-  (assert (member x (shuffle choices)))
-  (evaluate x (solve asserts)))
+(define (test-synth)
+  (define x (choose* (shuffle choices)))
+  (evaluate x (asserts)))
+  ;; (define binding
+  ;;   (synthesize #:forall (list x)
+  ;;               #:guarantee (= x 4)))
+  ;; (print-forms binding))
 
-(define (test-choose-one)
-  (clear-asserts!)
-  (define x (apply choose* (shuffle choices)))
-  (assert (< -4 x))
-  (evaluate x (solve asserts)))
 
-(define (agg-test num-trials pick-function)
-  (define accumulator (make-list 10 0))
-  (run-trials num-trials accumulator pick-function))
+;; (define (test-pick-one)
+;;   (clear-asserts!)
+;;   (define-symbolic* x integer?)
+;;   (assert (member x (shuffle choices)))
+;;   (evaluate x (solve asserts)))
 
-(define (run-trials num-trials accumulator pick-function)
-  (if (equal? 0 num-trials) accumulator
-      (run-trials (- num-trials 1)
-                  (list-update accumulator (pick-function)
-                               (lambda (count) (+ 1 count)))
-                  pick-function)))
+;; (define (test-choose-one)
+;;   (clear-asserts!)
+;;   (define x (apply choose* (shuffle choices)))
+;;   (assert (< -4 x))
+;;   (evaluate x (solve asserts)))
+
+;; (define (agg-test num-trials pick-function)
+;;   (define accumulator (make-list 10 0))
+;;   (run-trials num-trials accumulator pick-function))
+
+;; (define (run-trials num-trials accumulator pick-function)
+;;   (if (equal? 0 num-trials) accumulator
+;;       (run-trials (- num-trials 1)
+;;                   (list-update accumulator (pick-function)
+;;                                (lambda (count) (+ 1 count)))
+;;                   pick-function)))
