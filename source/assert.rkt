f1295ee722a88dfde274646206f26b399d75b7b0
Drew Bronson <bronsonboy96@gmail.com>
2017-03-15 18:10:29 -0500

    Change names of functions for clarity.

--- source/assert.rkt
+++ source/assert.rkt
@@ -1,17 +1,17 @@
 #lang rosette
 
-(provide equal not-equal)
+(provide equal! not-equal!)
 
 (define-syntax (p stx)
   (datum->syntax stx (reverse (cdr (syntax->datum stx)))))
 
-(define-syntax (not-equal stx)
+(define-syntax (not-equal! stx)
   (let* ([stx-list (syntax->datum stx)]
          [symbol (cadr stx-list)]
          [value (caddr stx-list)])
     (datum->syntax stx `(assert (not (equal? ,symbol ,value ))))))
 
-(define-syntax (equal stx)
+(define-syntax (equal! stx)
   (let* ([stx-list (syntax->datum stx)]
          [symbol (cadr stx-list)]
          [value (caddr stx-list)])
--- tests/assert-tests.rkt
+++ tests/assert-tests.rkt
@@ -4,9 +4,9 @@
 (require "../source/assert.rkt")
 
 ;; Equality Tests
-(check-exn exn:fail? (lambda () (equal 1 2)) "incorrect equality assertion")
-(check-not-exn (lambda() (equal 1 1)) "basic equality assertion")
+(check-exn exn:fail? (lambda () (equal! 1 2)) "incorrect equality assertion")
+(check-not-exn (lambda() (equal! 1 1)) "basic equality assertion")
 
 ;; Inequality Tests
-(check-exn exn:fail? (lambda () (not-equal 1 1)) "incorrect inequality assertion")
-(check-not-exn (lambda() (not-equal 1 2)))
+(check-exn exn:fail? (lambda () (not-equal! 1 1)) "incorrect inequality assertion")
+(check-not-exn (lambda() (not-equal! 1 2)))
