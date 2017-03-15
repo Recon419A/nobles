a23f21b48af3f8be096f8d21f3b8f2569ffc9fb2
Drew Bronson <bronsonboy96@gmail.com>
2017-03-15 18:17:48 -0500

    Add in! assertion.

--- source/assert.rkt
+++ source/assert.rkt
@@ -1,9 +1,15 @@
 #lang rosette
 
-(provide equal! not-equal!)
+(provide equal! not-equal! in!)
 
 (define-syntax (p stx)
   (datum->syntax stx (reverse (cdr (syntax->datum stx)))))
+
+(define-syntax (equal! stx)
+  (let* ([stx-list (syntax->datum stx)]
+         [symbol (cadr stx-list)]
+         [value (caddr stx-list)])
+    (datum->syntax stx `(assert (equal? ,symbol ,value )))))
 
 (define-syntax (not-equal! stx)
   (let* ([stx-list (syntax->datum stx)]
@@ -11,8 +17,8 @@
          [value (caddr stx-list)])
     (datum->syntax stx `(assert (not (equal? ,symbol ,value ))))))
 
-(define-syntax (equal! stx)
+(define-syntax (in! stx)
   (let* ([stx-list (syntax->datum stx)]
          [symbol (cadr stx-list)]
-         [value (caddr stx-list)])
-    (datum->syntax stx `(assert (equal? ,symbol ,value )))))
+         [possible-values (caddr stx-list)])
+    (datum->syntax stx `(assert (member ,symbol (shuffle ,possible-values ))))))
--- tests/assert-tests.rkt
+++ tests/assert-tests.rkt
@@ -3,10 +3,14 @@
 (require rackunit)
 (require "../source/assert.rkt")
 
-;; Equality Tests
+;; equal! tests
 (check-exn exn:fail? (lambda () (equal! 1 2)) "incorrect equality assertion")
 (check-not-exn (lambda() (equal! 1 1)) "basic equality assertion")
 
-;; Inequality Tests
+;; not-equal! tests
 (check-exn exn:fail? (lambda () (not-equal! 1 1)) "incorrect inequality assertion")
 (check-not-exn (lambda() (not-equal! 1 2)))
+
+;; in! tests
+(check-exn exn:fail? (lambda () (in! 1 '(2 3 4))))
+(check-not-exn (lambda () (in! 1 '(1 2 3))))
